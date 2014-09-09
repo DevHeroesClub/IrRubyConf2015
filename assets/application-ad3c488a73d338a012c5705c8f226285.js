4e13cc3902a8a0965c8f33daba63c44788648d91
Sameer Rahmani <lxsameer@gnu.org>
2014-09-09 17:13:50 +0430

    Release at 2014-09-09 12:43:50 UTC

---
+++ assets/application-ad3c488a73d338a012c5705c8f226285.js
@@ -0,0 +1,45 @@
+/*
+
+
+
+
+ */
+
+
+$(function() {
+    // Stick the #nav to the top of the window
+    var nav = $("#mainmenu");
+    var navHomeY = nav.offset().top;
+
+    var isFixed = false;
+    var $w = $(window);
+
+    $w.scroll(function() {
+        var scrollTop = $w.scrollTop();
+        var shouldBeFixed = scrollTop > navHomeY;
+        if (shouldBeFixed && !isFixed) {
+            nav.css({
+                position: 'fixed',
+                top: 0,
+                left: 0, //nav.offset().left,
+                width: '100%'//nav.width()
+            });
+            isFixed = true;
+            $("#toparrow").fadeIn();
+        }
+        else if (!shouldBeFixed && isFixed)
+        {
+            nav.css({
+                position: 'static'
+            });
+            isFixed = false;
+            $("#toparrow").fadeOut();
+        }
+
+    });
+
+     $(".ui.dropdown").dropdown();
+     $(".ui.sidebar").sidebar({overlay: true}).sidebar('attach events', '.toggle.btn');
+
+
+});
